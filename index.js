3f7264c3a1035c60b305fd894608b6859997648f
NaKia Whitby <whitby.nakia@gmail.com>
2018-05-26 17:06:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ const recipes = {
 };
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-object[key] = value
-return object;
+  object[key] = value;
+  return object;
 }
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({},object,{[key]: value})
+return Object.assign({},object,{[key]: value});
 }
 
+function deleteFromObjectByKey(object, key){
+  let newObject =  Object.assign({},object)  
+  delete newObject[key]
+  return newObjrct;
+}
+
+  function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
 
 
