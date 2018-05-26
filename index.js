7af5f3000209716b016af631e740f7f1d0fe2c58
NaKia Whitby <whitby.nakia@gmail.com>
2018-05-26 17:02:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 object[key] = value
 return object;
 }
+
+function upddateObjectWithKeyandValye(object, key,value) {
+  
+}
+
+
+
+
+
